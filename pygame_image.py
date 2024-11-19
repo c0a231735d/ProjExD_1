3f8c984c889891deacca51e6393a51cf71817416
taichan <c0a231735d@edu.teu.ac.jp>
2024-11-19 15:45:41 +0900

    変更２こうかとん画像の描画

--- pygame_image.py
+++ pygame_image.py
@@ -10,14 +10,15 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    kouka = pg.image.load("3.ping")
-    kouka = pg.transform.flip(kouka, True,False)
+    koukaton = pg.image.load("fig/3.png")
+    koukaton = pg.transform.flip(koukaton, True,False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(koukaton, [100,300])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
