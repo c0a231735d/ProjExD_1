ebaa02afdcbacc9ae7dba0e8c6475474feeaf11e
taichan <c0a231735d@edu.teu.ac.jp>
2024-11-19 16:29:43 +0900

    変更6こうかとんの移動

--- pygame_image.py
+++ pygame_image.py
@@ -3,30 +3,41 @@ import sys
 import pygame as pg
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
-    clock  = pg.time.Clock()
+    clock = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    bg_img2 = pg.transform.flip(bg_img, True,False)
-    koukaton = pg.image.load("fig/3.png")
-    koukaton = pg.transform.flip(koukaton, True,False)
+    bg_img2 = pg.transform.flip(bg_img, True, False)
+    koukaton_img = pg.image.load("fig/3.png")
+    koukaton_img = pg.transform.flip(koukaton_img, True, False)
+    koukaton_rect = koukaton_img.get_rect()
+    koukaton_rect.center = (300, 200)  
     tmr = 0
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: return
-        x = -(tmr%3200)
-        screen.blit(bg_img, [x, 0])
-        screen.blit(bg_img2, [tmr+1600, 0])
-        screen.blit(bg_img, [tmr+3200, 3200])
-        screen.blit(bg_img2, [tmr+4800, 0])
-        screen.blit(koukaton, [100,300])
-        pg.display.update()
-        tmr += 1        
-        clock.tick(200)
+            if event.type == pg.QUIT:
+                return
 
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:    
+            koukaton_rect.move_ip(0, -1)
+        if key_lst[pg.K_DOWN]:  
+            koukaton_rect.move_ip(0, 1)
+        if key_lst[pg.K_LEFT]:  
+            koukaton_rect.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]: 
+            koukaton_rect.move_ip(1, 0)
+        
+        x = -(tmr % 3200)
+        screen.blit(bg_img, [x, 0])
+        screen.blit(bg_img2, [x + 1600, 0])
+        screen.blit(bg_img, [x + 3200, 0])
+        screen.blit(bg_img2, [x + 4800, 0])
+        screen.blit(koukaton_img, koukaton_rect)
+        pg.display.update()
+        tmr += 1
+        clock.tick(200)
 
 if __name__ == "__main__":
     pg.init()
